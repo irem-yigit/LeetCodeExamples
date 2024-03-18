d327346d2b7e7ca40fd121ebe646691066e7cd24
Irem Yigit <yigitiremm@gmail.com>
2024-03-18 20:16:13 +0300

    codes updates.

--- AddTwoIntegers/src/AddTwoIntegers.java
+++ AddTwoIntegers/src/AddTwoIntegers.java
@@ -1,2 +1,9 @@
 public class AddTwoIntegers {
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(5,8));
+    }
+
 }
